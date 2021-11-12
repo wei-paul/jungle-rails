38ca1d3933b001c424c84e480532d4fd1aa1a273
wei-paul <paul.wei607@gmail.com>
2021-11-12 21:16:56 +0000

    Added admin/category functionality

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,27 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with :name => ENV["HTTP_BASIC_USER"], :password => ENV["HTTP_BASIC_PASSWORD"]
+
+  def index
+    @categories = Category.all
+  end
+  
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      redirect_to [:admin, :categories]
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+end
